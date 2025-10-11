c47ae9826b15d04fafee618240527dd25ea2ae79
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-10-10 22:49:49 -0300

    :sparkless: events crud

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EventsService {
+  constructor(private prismaService: PrismaService) {}
   create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+    return this.prismaService.events.create({
+      data: createEventDto,
+    });
   }
 
   findAll() {
-    return `This action returns all events`;
+    return this.prismaService.events.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  findOne(id: string) {
+    return this.prismaService.events.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
+  update(id: string, updateEventDto: UpdateEventDto) {
+    return this.prismaService.events.update({
+      where: {
+        id,
+      },
+      data: updateEventDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} event`;
+  remove(id: string) {
+    return this.prismaService.events.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
